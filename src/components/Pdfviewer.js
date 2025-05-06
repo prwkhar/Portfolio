293f8c609ff2705c6a94668f6c5d7e16649a5089
prakhar <prakharagarwal1434@gmail.com>
2025-05-06 14:53:20 +0530

    usual

--- src/components/Pdfviewer.js
+++ src/components/Pdfviewer.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Pdfviewer() {
   return (
-    <div className="flex flex-col items-center w-full m-4 justify-center h-[80vh] bg-transparent text-white">
+    <div className="flex flex-col items-center w-[95%] md:w-[70%] m-4 justify-center h-[500px] md:h-[80vh] bg-transparent text-white">
         <iframe
             src="https://drive.google.com/file/d/1Y9p33COfFydZoj_0gAchvJNi-E4dX5oU/preview"
-            width="60%"
-            height="400%"
+            width="100%"
+            height="100%"
             className='rounded-2xl shadow-lg shadow-teal-500/10'
         ></iframe></div>
   )
