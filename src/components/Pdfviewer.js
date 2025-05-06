f0952b29cd7a89a4f0bdf697a86273f0a42876d4
prakhar <prakharagarwal1434@gmail.com>
2025-05-06 14:48:50 +0530

    drive

--- src/components/Pdfviewer.js
+++ src/components/Pdfviewer.js
@@ -4,7 +4,7 @@ function Pdfviewer() {
   return (
     <div className="flex flex-col items-center w-full m-4 justify-center h-[80vh] bg-transparent text-white">
         <iframe
-            src="/cv.pdf"
+            src="https://drive.google.com/file/d/1Y9p33COfFydZoj_0gAchvJNi-E4dX5oU/preview"
             width="60%"
             height="400%"
             className='rounded-2xl shadow-lg shadow-teal-500/10'
