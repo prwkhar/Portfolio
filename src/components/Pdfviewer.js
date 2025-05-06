0afae7c9c58012bf504c152371715ff1bd75022e
prakhar <prakharagarwal1434@gmail.com>
2025-05-06 14:57:40 +0530

    updated

--- src/components/Pdfviewer.js
+++ src/components/Pdfviewer.js
@@ -4,7 +4,7 @@ function Pdfviewer() {
   return (
     <div className="flex flex-col items-center w-[95%] md:w-[70%] m-4 justify-center h-[500px] md:h-[80vh] bg-transparent text-white">
         <iframe
-            src="https://drive.google.com/file/d/1Y9p33COfFydZoj_0gAchvJNi-E4dX5oU/preview"
+            src="https://drive.google.com/file/d/1820E9p1QRYlb4rR9txlpncZUTw1LVW0U/preview"
             width="100%"
             height="100%"
             className='rounded-2xl shadow-lg shadow-teal-500/10'
